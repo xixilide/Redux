fe485cc549c1d5d64142fc72ca7fd83e8bc35e30
xixilide <754396775@qq.com>
2017-01-02 11:22:09 +0800

    hot

--- dev-env/src/store.js
+++ dev-env/src/store.js
@@ -14,3 +14,10 @@ const store = createStore(rootReducer, defaultState);
 
 export const history = syncHistoryWithStore(browserHistory, store);
 export default store;
+
+if(module.hot) {
+  module.hot.accept('./reducers/',() => {
+    const nextRootReducer = require('./reducers/index').default;
+    store.replaceReducer(nextRootReducer);
+  });
+}
